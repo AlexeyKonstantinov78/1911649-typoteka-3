91067b2e8e9ae7ecc00d294be4aa26bebc372fe2
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2022-03-02 15:48:02 +0300

    add Publication Generator

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {cli} = require(`./cli/index`);
+const {DEFAULT_COMMAND, USER_ARGV_INDEX, exitCode} = require(`../constants`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
+
+if (userArguments.length === 0 || !cli[userCommand]) {
+  cli[DEFAULT_COMMAND].run();
+  process.exit(exitCode.success);
+}
+
+cli[userCommand].run(userArguments.slice(1));
