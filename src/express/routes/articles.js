f44acbe06a686f22a1511bad0499ef6142887235
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2022-03-11 15:37:51 +0300

    update router

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 const articlesRouter = new Router();
 
 articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRouter.get(`/:id`, (req, res) => {res.send(`/articles/:id ${req.params.id}`)});
+articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/:id ${req.params.id}`));
 articlesRouter.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id ${req.params.id}`));
 articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id ${req.params.id}`));
 
