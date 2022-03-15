f337cbeb78c640876ebd69b5535dd419d7f4c366
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2022-03-15 10:07:19 +0300

    add pug upgrade express.js and html transform pug

--- src/express/express.js
+++ src/express/express.js
@@ -2,29 +2,39 @@
 
 const express = require(`express`);
 const app = express();
+const path = require(`path`);
 const DEFAULT_PORT = 8080;
-const chalk = require(`chalk`);
 
+const chalk = require(`chalk`);
+// роутер
 const articlesRouter = require(`./routes/articles`);
 const muRouter = require(`./routes/my`);
+
+// подключение шаблонизатора pug и каталог директория шаблонов
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+app.get(`/`, (req, res) => {
+  res.render(`main`);
+});
 
 app.use(`/articles`, articlesRouter);
 app.use(`/my`, muRouter);
 
-app.get(`/`, (req, res) => {
-  res.send(`/`);
-});
-
 app.get(`/register`, (req, res) => {
-  res.send(`/register`);
+  res.render(`sign-up`);
 });
 
 app.get(`/login`, (req, res) => {
-  res.send(`/login`);
+  res.render(`login`);
 });
 
 app.get(`/search`, (req, res) => {
-  res.send(`/search`);
+  res.render(`search/search`);
+});
+
+app.get(`/post`, (req, res) => {
+  res.render(`post-detail`);
 });
 
 app.listen(DEFAULT_PORT, () => console.log(chalk.green(`Сервер запущен на порту: ${DEFAULT_PORT}`)));
