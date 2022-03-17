1da75fbc127d0982e3a91ea4caf3481fa7c280bb
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2022-03-17 17:10:23 +0300

    Revert "add Another server"
    This reverts commit b29492af27a5c03c8b4f60aaf268e1ff7367f417.

--- src/express/express.js
+++ src/express/express.js
@@ -3,28 +3,39 @@
 const express = require(`express`);
 const app = express();
 const path = require(`path`);
-const chalk = require(`chalk`);
-
-// роутер
-const offersRoutes = require(`./routes/offers-routes`);
-const myRoutes = require(`./routes/my-routes`);
-const mainRoutes = require(`./routes/main-routes`);
-
-// константы
-const PUBLIC_DIR = `public`;
 const DEFAULT_PORT = 8080;
 
-// статичные передача данныx как стили скрипты шрифты картинки
-app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+const chalk = require(`chalk`);
+// роутер
+const articlesRouter = require(`./routes/articles`);
+const muRouter = require(`./routes/my`);
 
 // подключение шаблонизатора pug и каталог директория шаблонов
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
-// Подключим созданные маршруты
-app.use(`/offers`, offersRoutes);
-app.use(`/my`, myRoutes);
-app.use(`/`, mainRoutes);
+app.get(`/`, (req, res) => {
+  res.render(`main`);
+});
 
-// run server
+app.use(`/articles`, articlesRouter);
+app.use(`/my`, muRouter);
+
+app.get(`/register`, (req, res) => {
+  res.render(`sign-up`);
+});
+
+app.get(`/login`, (req, res) => {
+  res.render(`login`);
+});
+
+app.get(`/search`, (req, res) => {
+  res.render(`search/search`);
+});
+
+app.get(`/post`, (req, res) => {
+  res.render(`post-detail`);
+});
+
 app.listen(DEFAULT_PORT, () => console.log(chalk.green(`Сервер запущен на порту: ${DEFAULT_PORT}`)));
+
--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -5,5 +5,6 @@ const myRouter = new Router();
 
 myRouter.get(`/`, (req, res) => res.send(`/my`));
 myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
+myRouter.get(`/categories`, (req, res) => res.send(`/my/categories`));
 
 module.exports = myRouter;
