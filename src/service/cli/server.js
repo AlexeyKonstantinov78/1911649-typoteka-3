e6642410b1627475a465e408ad2f5f9fb8e174f0
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2022-03-30 21:04:18 +0300

    add logger server

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -3,8 +3,10 @@
 const express = require(`express`);
 const app = express();
 const chalk = require(`chalk`);
-// const http = require(`http`);
-// const fs = require(`fs`).promises;
+const {getLogger} = require(`../lib/logger`);
+const logger = getLogger({name: `api`});
+
+
 const {HttpCode, API_PREFIX} = require(`../../constants`);
 const routes = require(`../api`);
 const getMockData = require(`../lib/get-mock-data`);
@@ -12,6 +14,14 @@ const getMockData = require(`../lib/get-mock-data`);
 const DEFAULT_PORT = 3000;
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
 
 app.use(API_PREFIX, routes);
 
@@ -24,9 +34,15 @@ app.get(`/posts`, async (req, res) => {
   }
 });
 
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(`Not found`));
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND)
+    .send(`Not found`);
+  logger.error(`Route not found: ${req.url}`);
+});
+
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occurred on processing request: ${err.message}`);
+});
 
 module.exports = {
   name: `--server`,
@@ -35,13 +51,18 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    app
+    try {
+      app
       .listen(port)
       .on(`listening`, (err) => {
-        console.info(chalk.green(`Ожидаю соединений на ${port}`));
+        logger.info(`Ожидаю соединений на порт: ${port}`);
       })
       .on(`error`, ({message}) => {
-        console.error(chalk.red(`Ошибка при создании сервера: ${message}`));
+        logger.error(`Ошибка при создании сервера: ${message}`);
       });
+    } catch (err) {
+      logger.error(`Произошла ошибка ${err.message}`);
+      process.exit(1);
+    }
   }
 };
