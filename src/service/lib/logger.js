c13f9ec227f4c7c0105363eb501d8403265e0956
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2022-03-31 09:10:38 +0300

    update logger

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -3,20 +3,10 @@
 const pino = require(`pino`);
 const {Env} = require(`../../constants`);
 
-console.log(Env);
-
 const LOG_FILE = `./logs/api.log`;
 const isDevMode = process.env.NODE_ENV === Env.DEVELOPMENT;
 
-console.log(`isDevMode: ` + isDevMode);
-console.log(`LOG_LEVEL: ` + process.env.LOG_LEVEL);
-console.log(`NODE_ENV: ` + process.env.NODE_ENV);
-console.log(`Env.DEVELOPMENT: `, Env.DEVELOPMENT);
-
 const defaultLogLevel = isDevMode ? `debug` : `error`;
-
-console.log('defaultLogLeve: ', defaultLogLevel);
-console.log(`logger.level: `, process.env.LOG_LEVEL || defaultLogLevel);
 
 const logger = pino({
   name: `base-logger`,
