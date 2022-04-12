afee718f39a5bda0c406506c2290bb0a5b731a58
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2022-04-12 21:50:48 +0300

    upgrade file fill-db.sql

---
+++ fill-db.sql
@@ -0,0 +1,19 @@
+-- заполняем таблицу users
+INSERT INTO users(email, password_hash, first_name, last_name, avatar) VALUES
+('ivanov@example.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Иван', 'Иванов', 'avatar1.jpg'),
+('petrov@example.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Пётр', 'Петров', 'avatar2.jpg');
+
+-- заполняем таблицу categories
+
+INSERT INTO categories(name) VALUES
+(`Деревья`),
+(`За жизнь`)
+(`Без рамки`),
+(`Разное`),
+(`IT`),
+(`Музыка`),
+(`Кино`),
+(`Программирование`),
+(`Железо`);
+
+INSERT INTO articles()
