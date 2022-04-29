ad59c9c2e1cb51e5cd1f1ed942f549415dc84178
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2022-04-29 16:02:54 +0300

    refactor: api and data-servis

--- src/service/data-service/search.js
+++ src/service/data-service/search.js
@@ -1,14 +1,27 @@
 'use strict';
 
+const {Op} = require(`sequelize`);
+const Aliase = require(`../models/aliase`);
+
 class SearchService {
-  constructor(offers) {
-    this._offers = offers;
+  constructor(sequelize) {
+    this._Article = sequelize.models.Article;
   }
 
-  findAll(searchText) {
-    const offer = this._offers.filter((item) => item.title == searchText);
+  async findAll(searchText) {
+    const articles = await this._Article.findAll({
+      where: {
+        title: {
+          [Op.substring]: searchText
+        }
+      },
+      include: [Aliase.CATEGORIES],
+      order: [
+        [`createdAt`, `DESC`]
+      ]
+    });
 
-    return offer;
+    return articles.map((article) => article.get());
   }
 }
 
